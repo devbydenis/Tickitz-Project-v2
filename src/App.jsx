4fa47b414e4836679388dcaa6d8ddda9f082516a
devbydenis <hello.denisrahmadi@gmail.com>
2025-03-13 05:06:53 +0700

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Button from "./components/Button"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Login from "./pages/login"
+
+
 function App() {
 
   return (
     <>
-    
+      <Navbar />
+      {/* <Login /> */}
+      <Footer />
     </>
   )
-}
+} 
 
 export default App
