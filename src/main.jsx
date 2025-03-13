286ff95ef5e7a4757f9e4b8250531d9d66599d07
devbydenis <hello.denisrahmadi@gmail.com>
2025-03-14 04:58:09 +0700

    feat: add a few router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Account from "./pages/profile/Account.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<App />}/>
+      {/* <Route index element={<Home />}/> */}
+      <Route path="profile">
+        <Route index element={<Account />}/>
+        <Route path="history" element={<History />}/>
+      </Route>
+      <Route path="register" element={<Register />}/>
+      <Route path="login"  element={<Login />}/>
+    </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
