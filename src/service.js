355e632ab53a9a66135952e2a9e0af08291e9d85
devbydenis <hello.denisrahmadi@gmail.com>
2025-03-17 06:25:58 +0700

    feat: add env api key

--- src/service.js
+++ src/service.js
@@ -1,3 +1,4 @@
+export const API_KEY = import.meta.env.VITE_API_KEY
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 const urlNowPlayingMovie = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'
 const urlUpComingMovies = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"
@@ -6,7 +7,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZmM3ZWNhZjdjYjAzMTk3MmM4ODFhYzA5Y2MzNGE2YSIsIm5iZiI6MTc0MTMxMzM1OS45NjcsInN1YiI6IjY3Y2E1NTRmNzQ3OWQ4Yzg0OTJiM2Q2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GrBEVi__prOYL5AB5KMgbg0dvTc3I6Ar6cEfl29M5yE'
+    Authorization: `Bearer ${API_KEY}`
   }
 };
 
