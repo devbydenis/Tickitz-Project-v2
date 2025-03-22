d62ae72ad2fe2115c235b3fa04760caa3c20e81d
devbydenis <hello.denisrahmadi@gmail.com>
2025-03-22 22:25:18 +0700

    feat: add h1

--- src/pages/admin/AddMovie.jsx
+++ src/pages/admin/AddMovie.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function AddMovie() {
   return (
-    <div>AddMovie</div>
+    <>
+      <section className=''>
+        <h1 className='text-2xl'>Add New Movie</h1>
+      </section>
+    </>
   )
 }
 
